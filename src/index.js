99a1dbb44790295ad4251225e5a1b045efb43c1e
IslamusWisefool <97357305+IslamusWisefool@users.noreply.github.com>
2023-05-26 19:17:05 +0600

    Added production build

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import "./index.html";
+import "./index.scss";
+// import code from './img/code.png'
+// import { mult, sum } from './modules/calc';
+
+// const title = document.querySelector(".header-title");
+
+// document.addEventListener("mousemove", (e) => {
+//   titleRotate(e, title);
+// });
+
+// function titleRotate(event, element) {
+//   const x = event.clientX;
+//   const y = event.clientY;
+
+//   const middleX = window.innerWidth / 2;
+//   const middleY = window.innerHeight / 2;
+
+//   const offsetX = ((x - middleX) / middleX) * 45;
+//   const offsetY = ((y - middleY) / middleY) * 45;
+
+//   element.style.setProperty("--rotateX", offsetX + "deg");
+//   element.style.setProperty("--rotateY", -1 * offsetY + "deg");
+// }
